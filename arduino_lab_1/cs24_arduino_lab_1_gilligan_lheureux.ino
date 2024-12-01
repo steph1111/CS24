e9ec28560af752b6cfa92cec5734a9c6fc0f63a4
steph1111 <stephanielh1111@gmail.com>
2024-11-30 21:49:58 -0800

    Function comments

--- arduino_lab_1/cs24_arduino_lab_1_gilligan_lheureux.ino
+++ arduino_lab_1/cs24_arduino_lab_1_gilligan_lheureux.ino
@@ -1,9 +1,26 @@
+/**
+ * CS24: First Arduino Lab
+ * @authors Liam Gilligan and Stephanie L'Heureux
+ * 
+ * Your name in lights.
+ * Lights up seven LEDs in a pattern comprising of the seven least 
+ * significant bits of the ascii values in the letters of lab partners 
+ * names followed by a "space" (indicated by all lights off.)
+ */
 #include <string.h>
-#define BUTTON 9
 
+#define BUTTON 9
 String liam = "liam";
 String steph = "steph";
 
+/**
+ * Converts a character to binary.
+ * 
+ * @param c Charater to convert.
+ * @return Integer pointer to a heap allocated integer array containing
+ * seven elements, each either 1 or 0, representing the bits of the ASCII
+ * representation of the input character `c`.
+ */
 int* charbin(char c) {
   int* bin = new int[7];
   for (int i = 0; i < 7; i++) {
@@ -13,6 +30,14 @@ int* charbin(char c) {
   return bin;
 }
 
+/**
+ * Lights up a sequence of seven LEDs (pins 2-8) encoding the input parameter
+ * `name` as a pattern in which each character of `name` is displayed as 
+ * he least significant bits of its ASCII value. Characters are advanced
+ * by button press.
+ * 
+ * @param name String to display in lights.
+ */
 void display_name(String name) {
   for (int i = 0; i < name.length(); i++) {
     int* bin = charbin(name[i]);
@@ -24,21 +49,30 @@ void display_name(String name) {
   }
 }
 
+/**
+ * Delays execution until button has been pressed.
+ */
 void wait_for_button() {
-  bool lastValue = 1;
-  bool value = 1;
+  bool lastValue = digitalRead(BUTTON);
+  bool value = digitalRead(BUTTON);
   while(!value || lastValue) {
     lastValue = value;
     value = digitalRead(BUTTON);
   }
 }
 
+/**
+ * Sets all LEDs (pins 2-8) to LOW, turning them off.
+ */
 void set_low() {
   for (int i = 2; i < 9; i++) {
     digitalWrite(i, LOW);
   }
 }
 
+/**
+ * Initialize pin modes of each pin. 
+ */
 void setup() {
   for (int i = 2; i < 9; i++) {
     pinMode(i, OUTPUT);
@@ -46,6 +80,9 @@ void setup() {
   pinMode(BUTTON, INPUT);
 }
 
+/**
+ * Loop body.
+ */
 void loop() {
   display_name(liam);
   set_low();
